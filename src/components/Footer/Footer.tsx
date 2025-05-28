7e5fc7bed6b1c85180a8ea7b003f05e816eeb7e0
gkantzos <gkantzosg@gmail.com>
2025-05-28 18:47:02 +0300

    Deleted facebook

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -17,7 +17,7 @@ const Footer: React.FC = () => (
     </div>
 
     <div className={styles.socials}>
-      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+      
       <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
       <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
     </div>
